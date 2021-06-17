7ffc1c9a4d053d6e7b05670bc0e99924ed809e49
leiteguitar <85917335+leiteguitar@users.noreply.github.com>
2021-06-16 23:04:21 -0300

    Exercicio 1 - C Sharpers

---
+++ Exercicio1_CalculadorMeses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio1_CalculadorMeses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de Meses!");
+
+            Console.WriteLine("Digite o valor em meses: ");
+
+            string meses = Console.ReadLine();
+            int mesesCalculos = Convert.ToInt32(meses);
+
+            double resultado = mesesCalculos * 30;
+
+            Console.WriteLine("O valor em dias é: " + resultado);
+
+        }
+    }
+}
